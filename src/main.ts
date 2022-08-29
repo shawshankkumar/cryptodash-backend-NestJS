189f63d218b03dde19a7db3d5a72aaa050e08ffe
shawshankkumar <shashankkumarthakur@gmail.com>
2022-08-29 22:51:50 +0530

    ➕ Add modules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+  app.use(helmet());
+  app.setGlobalPrefix('api',{ exclude: ['health'] });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }));
+  await app.listen(process.env.PORT);
 }
 bootstrap();
